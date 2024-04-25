c006d50febc33696bc6c35311fc6a7488a2c8f05
ductho05 <ductho23pro@gmail.com>
2024-04-25 19:48:05 +0700

    update google

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/layouts/navbar/Navbar'
 import Footer from '@/components/layouts/footer/Footer'
 import CallButton from '@/components/CallButton/CallButton'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,6 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="favicon.ico" />
         <meta name="google-site-verification" content="oyZKGfWq3_JK_esth-rDJyV4eqOBnm7W1BMVLGhYTXw" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16518729039">
-        </script>
-        <script src='./google.js'></script>
       </head>
       <body className={inter.className}>
         <Navbar />
@@ -28,6 +26,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="AW-16518729039" />
     </html>
   )
 }
