4664f10a80c55c365cb102aaff79e50a98bbacef
ductho05 <ductho23pro@gmail.com>
2025-03-13 22:55:55 +0700

    update sitemap

--- src/app/robots.js
+++ src/app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://taxi-buon-ma-thuot.io.vn/sitemap.xml',
+        sitemap: 'https://taxibuonmathuot.vn/sitemap.xml',
     }
 }
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,19 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn',
+            url: 'https://taxibuonmathuot.vn',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn/about',
+            url: 'https://taxibuonmathuot.vn/about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://taxi-buon-ma-thuot.io.vn/contact',
+            url: 'https://taxibuonmathuot.vn/contact',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
